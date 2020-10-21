0f87e512337eabcc3a5c4d489fef0ef014c28cfb
tatauai <73146109+tatauai@users.noreply.github.com>
2020-10-21 20:59:24 -0300

    primeiro commit

---
+++ app/src/main/java/com/wcc/lancedados/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.wcc.lancedados
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val botaoLancarDados = findViewById<Button>(R.id.lanceDadosBotao)
+        val valorDado1 = findViewById<TextView>(R.id.dado1)
+        val valorDado2 = findViewById<TextView>(R.id.dado2)
+
+        botaoLancarDados.setOnClickListener {
+            valorDado1.text = larcarDados().toString()
+            valorDado2.text = larcarDados().toString()
+
+        }
+    }
+
+    fun larcarDados(): Int {
+
+        return (1..6).random();
+    }
+}
