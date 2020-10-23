847cc52b6abf41f3ddef5948251e0b85f423aa48
tatauai <73146109+tatauai@users.noreply.github.com>
2020-10-22 22:12:13 -0300

    adicionando imagens dos dados

--- app/src/main/java/com/wcc/lancedados/MainActivity.kt
+++ app/src/main/java/com/wcc/lancedados/MainActivity.kt
@@ -11,11 +11,9 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val botaoLancarDados = findViewById<Button>(R.id.lanceDadosBotao)
-        val valorDado1 = findViewById<TextView>(R.id.dado1)
-        val valorDado2 = findViewById<TextView>(R.id.dado2)
+
 
         botaoLancarDados.setOnClickListener {
-            valorDado1.text = larcarDados().toString()
 
 
         }
