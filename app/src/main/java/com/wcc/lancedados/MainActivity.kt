c09a45a6ac44326c07492f28ce75a899b49895f9
tatauai <73146109+tatauai@users.noreply.github.com>
2020-10-23 22:06:05 -0300

    criando função para as imageViews

--- app/src/main/java/com/wcc/lancedados/MainActivity.kt
+++ app/src/main/java/com/wcc/lancedados/MainActivity.kt
@@ -3,6 +3,7 @@ package com.wcc.lancedados
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
@@ -11,16 +12,29 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val botaoLancarDados = findViewById<Button>(R.id.lanceDadosBotao)
-
+        val dado1 = findViewById<ImageView>(R.id.dado1)
+        val dado2 = findViewById<ImageView>(R.id.dado2)
 
         botaoLancarDados.setOnClickListener {
-
-
+            val numero = larcarDados()
+            dado1.setImageResource(escolherImagem(numero))
+            dado2.setImageResource(escolherImagem(numero))
         }
     }
 
-    fun larcarDados(): Int {
+    fun escolherImagem(numero:Int): Int{
+        return when(numero){
+            1-> R.drawable.dice_1
+            2-> R.drawable.dice_2
+            3-> R.drawable.dice_3
+            4-> R.drawable.dice_4
+            5-> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
 
+    }
+
+    fun larcarDados(): Int {
         return (1..6).random();
     }
 }
